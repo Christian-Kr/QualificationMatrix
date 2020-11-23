11c1073db1820ed9a8f9c03f45218e5ab60c1f01
Christian Kr <CerebrosuS_aedd_gmx.net>
2020-11-23 19:50:00 +0100

    Add integrity check controller

---
+++ src/certificate/qmcertificateintegritycheckdialog.cpp
@@ -0,0 +1,78 @@
+//
+// qmcertificateintegritycheckdialog.cpp is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+// the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "qmcertificateintegritycheckdialog.h"
+#include "ui_qmcertificateintegritycheckdialog.h"
+#include "model/qmdatamanager.h"
+#include "settings/qmapplicationsettings.h"
+
+#include <QSqlTableModel>
+
+#include <QDebug>
+
+QMCertificateIntegrityCheckDialog::QMCertificateIntegrityCheckDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    ui = new Ui::QMCertificateIntegrityCheckDialog;
+    ui->setupUi(this);
+
+    updateData();
+}
+
+QMCertificateIntegrityCheckDialog::~QMCertificateIntegrityCheckDialog()
+{
+    delete ui;
+}
+
+void QMCertificateIntegrityCheckDialog::accept()
+{
+    // TODO: Run check.
+
+    QDialog::accept();
+}
+
+void QMCertificateIntegrityCheckDialog::closeEvent(QCloseEvent *event)
+{
+    saveSettings();
+
+    QDialog::closeEvent(event);
+}
+
+void QMCertificateIntegrityCheckDialog::saveSettings()
+{
+    auto &settings = QMApplicationSettings::getInstance();
+
+    // Window settings.
+    settings.write("CertificateIntegrityCheckDialog/Width", width());
+    settings.write("CertificateIntegrityCheckDialog/Height", height());
+}
+
+void QMCertificateIntegrityCheckDialog::updateData()
+{
+    // Get the model data.
+    auto dm = QMDataManager::getInstance();
+
+    certificateModel = dm->getCertificateModel();
+}
+
+void QMCertificateIntegrityCheckDialog::keyPressEvent(QKeyEvent *event)
+{
+    if (event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return)
+    {
+        return;
+    }
+
+    QDialog::keyPressEvent(event);
+}
---
+++ src/certificate/qmcertificateintegritycheckdialog.h
@@ -0,0 +1,66 @@
+//
+// qmcertificateintegritycheckdialog.h is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+// the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef QMCERTIFICATEINTEGRITYCHECKDIALOG_H
+#define QMCERTIFICATEINTEGRITYCHECKDIALOG_H
+
+#include <QDialog>
+#include <memory>
+
+// Forward declaration.
+class QSqlTableModel;
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+class QMCertificateIntegrityCheckDialog;
+}
+QT_END_NAMESPACE
+
+/// Show dialog for integrity check of certificate database.
+/// \author Christian Kr, Copyright 2020
+class QMCertificateIntegrityCheckDialog: public QDialog
+{
+Q_OBJECT
+
+public:
+    /// Constructor
+    /// \param parent The parent object of the widgt. This is important for a modal dialog.
+    explicit QMCertificateIntegrityCheckDialog(QWidget *parent = nullptr);
+
+    /// Destructor
+    ~QMCertificateIntegrityCheckDialog() override;
+
+    /// Save settings from the dialog.
+    void saveSettings();
+
+protected:
+    /// Override from QDialog.
+    void closeEvent(QCloseEvent *event) override;
+
+    /// Override from QDialog.
+    void keyPressEvent(QKeyEvent *event) override;
+
+public slots:
+    /// Update the models, cause they might have changed.
+    void updateData();
+
+private:
+    Ui::QMCertificateIntegrityCheckDialog *ui;
+
+    std::shared_ptr<QSqlTableModel> certificateModel;
+};
+
+#endif // QMCERTIFICATEINTEGRITYCHECKDIALOG_H
