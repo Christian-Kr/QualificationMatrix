595c5f5ef05e7f2bf491e76cbf6ddfee115a3012
Christian Kr <CerebrosuS_aedd_gmx.net>
2021-01-16 17:51:45 +0100

    Don't save signing list image in database

--- database/sql_1_1.sql
+++ database/sql_1_1.sql
@@ -1,7 +1,3 @@
--- Add option in info for file_location, which is necessary for certificates
-
-INSERT INTO "Info" ("name", "value") VALUES("signinglist_image", "");
-
 -- Change Employee to mark an employee as active or not
 
 ALTER TABLE "Employee" ADD COLUMN "active" INTEGER default 1;
