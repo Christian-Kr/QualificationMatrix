8f75ef721b89c22225c8ead909d56b73a97e0900
Christian Kr <CerebrosuS_aedd_gmx.net>
2021-01-17 16:09:10 +0100

    Add framework dialog

---
+++ src/framework/qmdialog.cpp
@@ -0,0 +1,41 @@
+//
+// qmdialog.cpp is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+// the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "qmdialog.h"
+
+#include <QKeyEvent>
+
+QMDialog::QMDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    /// Be sure only settings will be called, that are available during this state.
+    loadSettings();
+}
+
+void QMDialog::closeEvent(QCloseEvent *event)
+{
+    saveSettings();
+    QDialog::closeEvent(event);
+}
+
+void QMDialog::keyPressEvent(QKeyEvent *event)
+{
+    if (event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return)
+    {
+        return;
+    }
+
+    QDialog::keyPressEvent(event);
+}
---
+++ src/framework/qmdialog.h
@@ -0,0 +1,48 @@
+//
+// qmdialog.h is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+// the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef QMDIALOG_H
+#define QMDIALOG_H
+
+#include <QDialog>
+
+/// General dialog class which implements functionalities, that should be available in all other
+/// dialogs.
+/// \author Christian Kr <CerebrosuS_aedd_gmx.net>
+class QMDialog: public QDialog
+{
+Q_OBJECT
+
+public:
+    /// Constructor
+    /// \param parent
+    explicit QMDialog(QWidget *parent = nullptr);
+
+    /// Will be called from closeEvent.
+    virtual void saveSettings() {};
+
+    /// Will be called from constructor.
+    virtual void loadSettings() {};
+
+    /// Override from QDialog. Do several actions on close.
+    /// \param event
+    void closeEvent(QCloseEvent *event) override;
+
+protected:
+    /// Override from QDialog. Permit close dialog on pressing enter.
+    void keyPressEvent(QKeyEvent *event) override;
+};
+
+#endif // QMDIALOG_H
