c4d584a85bd92b944b3c2bfe2a05a44d93736fb3
Christian Kr <CerebrosuS_aedd_gmx.net>
2021-04-17 16:04:40 +0200

    add object for future json support

---
+++ src/appcache/appcacheconfigjson.h
@@ -0,0 +1,31 @@
+// applcacheconfigjson.h is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef QUALIFICATIONMATRIX_APPCACHECONFIGJSON_H
+#define QUALIFICATIONMATRIX_APPCACHECONFIGJSON_H
+
+#include "appcacheconfig.h"
+
+/// Add support for configurations written in json format.
+/// \author Christian Kr, Copyright 2021
+class AppCacheConfigJSON: public AppCacheConfig
+{
+    Q_OBJECT
+
+public:
+    /// Constructor
+    /// \param parent
+    explicit AppCacheConfigJSON(QObject *parent = nullptr);
+};
+
+#endif //QUALIFICATIONMATRIX_APPCACHECONFIGJSON_H
