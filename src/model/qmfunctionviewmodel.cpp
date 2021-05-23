50f7308ea3d86a1ee157ab816173695c69df0481
Christian Kr <CerebrosuS_aedd_gmx.net>
2021-05-23 10:10:04 +0200

    add function view model

---
+++ src/model/qmfunctionviewmodel.cpp
@@ -0,0 +1,32 @@
+// qmfunctionviewmodel.cpp is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
+
+#include "qmfunctionviewmodel.h"
+
+#include <QDebug>
+
+QMFunctionViewModel::QMFunctionViewModel(QObject *parent, const QSqlDatabase &db)
+    : QSqlTableModel(parent, db)
+{}
+
+void QMFunctionViewModel::initModel()
+{
+    // The name of the Table.
+    setTable("FuncView");
+
+    // Specify header data of table.
+    setHeaderData(1, Qt::Horizontal, tr("Name"));
+    setHeaderData(2, Qt::Horizontal, tr("Gruppe"));
+
+    sort(1, Qt::AscendingOrder);
+}
---
+++ src/model/qmfunctionviewmodel.h
@@ -0,0 +1,35 @@
+// qmfunctionviewmodel.h is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef QMFUNCTIONVIEWMODEL_H
+#define QMFUNCTIONVIEWMODEL_H
+
+#include <QSqlTableModel>
+
+/// Sql model for the function view table.
+/// \author Christian Kr, Copyright 2021
+class QMFunctionViewModel: public QSqlTableModel
+{
+Q_OBJECT
+
+public:
+    /// Constructor
+    /// \param parent Parent object for the qt system.
+    /// \param db The database to work with.
+    explicit QMFunctionViewModel(QObject *parent = nullptr, const QSqlDatabase &db = QSqlDatabase());
+
+    /// Override from QMSqlRelationalTableModel.
+    void initModel() override;
+};
+
+#endif // QMFUNCTIONVIEWMODEL_H
