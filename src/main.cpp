2f49debcbc14c7b2853d970bcb780394d7a1e848
Christian Kr <CerebrosuS_aedd_gmx.net>
2021-08-13 21:24:09 +0200

    code style

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 ﻿// main.cpp is part of QualificationMatrix
 //
-// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of the GNU General
-// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
+// QualificationMatrix is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
 //
-// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-// details.
+// QualificationMatrix is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
 //
-// You should have received a copy of the GNU General Public License along with QualificationMatrix.
-// If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along
+// with QualificationMatrix. If not, see <http://www.gnu.org/licenses/>.
 
 #include "general/qmmainwindow.h"
 #include "settings/qmapplicationsettings.h"
@@ -38,8 +40,8 @@ void initShowMainWindow(QMainWindow &mainWin)
     auto &settings = QMApplicationSettings::getInstance();
     auto showMaximized = settings.read("MainWin/Maximized", false).toBool();
 
-    // Load the active screen from settings and put the window on this screen. Detection of the
-    // right screen is done by the screen name.
+    // Load the active screen from settings and put the window on this screen.
+    // Detection of the right screen is done by the screen name.
     QScreen *screen;
     {
         auto screens = QGuiApplication::screens();
@@ -81,8 +83,9 @@ void initShowMainWindow(QMainWindow &mainWin)
         auto savedHeight = settings.read("MainWin/Height", 500).toInt();
 
         mainWin.resize(savedWidth, savedHeight);
-        mainWin.move(screen->geometry().x() + (screen->geometry().width() - savedWidth) / 2,
-            screen->geometry().y() + (screen->geometry().height() - savedHeight) / 2);
+        mainWin.move(screen->geometry().x() + (screen->geometry().width() -
+            savedWidth) / 2, screen->geometry().y() +
+            (screen->geometry().height() - savedHeight) / 2);
 
         mainWin.show();
     }
@@ -97,8 +100,9 @@ void initApplicationStyle()
     QApplication::setStyle(style);
 }
 
-/// Install the translator into the qt application. If the loading or installing of the translation certificate fails,
-/// a warning message will be send for documentation.
+/// Install the translator into the qt application. If the loading or
+/// installing of the translation certificate fails, a warning message will
+/// be send for documentation.
 /// \param name The name of the translator certificate.
 /// \param path The path, where the tranaltor certificate could be found.
 void installTranslator(const QString &name, const QString &path)
@@ -108,17 +112,20 @@ void installTranslator(const QString &name, const QString &path)
 
     if (loaded)
     {
-        // Info: Installing certificate fails, if there is no translation in ts certificate itself.
+        // Info: Installing certificate fails, if there is no translation in
+        // ts certificate itself.
         auto installed = QApplication::installTranslator(translator);
 
         if (!installed)
         {
-            qWarning() << "cannot install language certificate" << name << "in" << path;
+            qWarning() << "cannot install language certificate" << name
+                << "in" << path;
         }
     }
     else
     {
-        qWarning() << "cannot load custom language certificate" << name << "in" << path;
+        qWarning() << "cannot load custom language certificate" << name
+            << "in" << path;
     }
 }
 
@@ -130,21 +137,26 @@ void initApplicationTranslation()
 
     installTranslator("qm_" + lang + ".qm", QDir::currentPath());
 
-    // Qt and qtbase translation files come from installed qt version or they need to be deployed
-    // with the application and its libraries.
-    installTranslator("qt_" + lang, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-    installTranslator("qtbase_" + lang, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    // Qt and qtbase translation files come from installed qt version or they
+    // need to be deployed with the application and its libraries.
+    installTranslator("qt_" + lang,
+        QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    installTranslator("qtbase_" + lang,
+        QLibraryInfo::location(QLibraryInfo::TranslationsPath));
 }
 
-/// Take all messages coming from qWarning, qDebug etc. and write them into a log certificate. In general, all message
-/// will be written into a log certificate. If this log certificate is not writeable, the message will be printed on
-/// stdout. Debug messages will always only be printed on stdout.
+/// Take all messages coming from qWarning, qDebug etc. and write them into a
+/// log certificate. In general, all message will be written into a log
+/// certificate. If this log certificate is not writeable, the message will be
+/// printed on stdout. Debug messages will always only be printed on stdout.
 ///
-/// The certificate file will be create in user directory, to be readable and individual by every user.
+/// The certificate file will be create in user directory, to be readable and
+/// individual by every user.
 ///
 /// \param type The message type (debug, warning etc.).
 /// \param msg The message to display/log.
-void customMessageHandler(QtMsgType type, const QMessageLogContext &, const QString &msg)
+void customMessageHandler(QtMsgType type, const QMessageLogContext &,
+    const QString &msg)
 {
     auto txt = QDateTime::currentDateTime().toString();
 
@@ -199,11 +211,13 @@ int main(int argc, char *argv[])
     // General information, used by qt - example: QSettings certificate naming
     QCoreApplication::setOrganizationName("Kr");
     QCoreApplication::setOrganizationDomain("Kr");
-    QCoreApplication::setApplicationVersion(QString("%1.%2").arg(VERSION_MAJOR, VERSION_MINOR));
+    QCoreApplication::setApplicationVersion(
+            QString("%1.%2").arg(VERSION_MAJOR, VERSION_MINOR));
 
     if (QString(RELEASE_STATE).compare("beta") == 0)
     {
-        QCoreApplication::setApplicationName(QString("QualificationMatrix_%1").arg(RELEASE_STATE));
+        QCoreApplication::setApplicationName(
+                QString("QualificationMatrix_%1").arg(RELEASE_STATE));
     }
     else
     {
