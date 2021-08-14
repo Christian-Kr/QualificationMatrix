710c70a46eb6c9a092917d2a90f50242ebcca18c
Christian Kr <CerebrosuS_aedd_gmx.net>
2021-08-14 20:02:24 +0200

    code style

--- src/ams/qmamslogindialog.cpp
+++ src/ams/qmamslogindialog.cpp
@@ -1,15 +1,17 @@
 // qmamssettingsdialog.cpp is part of QualificationMatrix
 //
-// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of the GNU General
-// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
+// QualificationMatrix is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
 //
-// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-// details.
+// QualificationMatrix is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
 //
-// You should have received a copy of the GNU General Public License along with QualificationMatrix.
-// If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along
+// with QualificationMatrix. If not, see <http://www.gnu.org/licenses/>.
 
 #include "qmamslogindialog.h"
 #include "ui_qmamslogindialog.h"
@@ -45,14 +47,17 @@ void QMAMSLoginDialog::login()
 
     if (am->getLoginUserName()->compare(username) == 0)
     {
-        QMessageBox::information(this, tr("Anmelden"), tr("Benutzer ist bereits angemeldet."));
+        QMessageBox::information(this, tr("Anmelden"),
+                tr("Benutzer ist bereits angemeldet."));
         return;
     }
 
     if (am->getLoginState() == LoginState::LOGGED_IN)
     {
-        auto res = QMessageBox::information(this, tr("Anmelden"), tr("Ein Benutzer ist bereits angemeldet und wird "
-                "mit dem login abgemeldet. Fortfahren?"), QMessageBox::Yes | QMessageBox::No);
+        auto res = QMessageBox::information(this, tr("Anmelden"),
+                tr("Ein Benutzer ist bereits angemeldet und wird "
+                "mit dem login abgemeldet. Fortfahren?"),
+                QMessageBox::Yes | QMessageBox::No);
 
         if (res == QMessageBox::No)
         {
@@ -65,7 +70,8 @@ void QMAMSLoginDialog::login()
     // Test afterwarts, whether everything goes right.
     if (am->getLoginUserName()->compare(username) != 0)
     {
-        QMessageBox::information(this, tr("Anmelden"), tr("Bei der Anmeldung is etwas schief gelaufen."));
+        QMessageBox::information(this, tr("Anmelden"),
+                tr("Bei der Anmeldung is etwas schief gelaufen."));
         return;
     }
 }
