01cc22d98a14717f565d6a3a058b9b06482c3952
Christian Kr <CerebrosuS_aedd_gmx.net>
2021-08-14 22:03:58 +0200

    add more ams models

---
+++ src/ams/model/qmamsgroupmodel.h
@@ -0,0 +1,38 @@
+// qmamsgroupmodel.h is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with QualificationMatrix. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef QMAMSGROUPMODEL_H
+#define QMAMSGROUPMODEL_H
+
+#include "framework/qmsqltablemodel.h"
+
+/// Group table for the access management system.
+/// \author Christian Kr, Copyright 2020
+class QMAMSGroupModel: public QMSqlTableModel
+{
+    Q_OBJECT
+
+public:
+    /// Constructor
+    /// \param parent Parent object for qt system.
+    /// \param db The database object to work with.
+    explicit QMAMSGroupModel(QObject *parent = nullptr,
+            const QSqlDatabase &db = QSqlDatabase());
+
+    /// Override from QMSqlTableModel.
+    void initModel() override;
+};
+
+#endif // QMAMSGROUPMODEL_H
---
+++ src/ams/model/qmamsusergroupmodel.cpp
@@ -0,0 +1,37 @@
+// qmamsusergroupmodel.cpp is part of QualificationMatrix
+//
+// QualificationMatrix is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// QualificationMatrix is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with QualificationMatrix. If not, see <http://www.gnu.org/licenses/>.
+
+#include "qmamsusergroupmodel.h"
+
+QMAMSUserGroupModel::QMAMSUserGroupModel(QObject *parent,
+        const QSqlDatabase &db)
+    : QMSqlTableModel(parent, db)
+{
+    initModel();
+}
+
+void QMAMSUserGroupModel::initModel()
+{
+    // The name of the Table.
+    setTable("AMSUserGroup");
+
+    // The edit and join mode/strategy.
+    setJoinMode(QSqlRelationalTableModel::LeftJoin);
+    setEditStrategy(QSqlTableModel::OnManualSubmit);
+
+    // Specifiy header data of table.
+    setHeaderData(1, Qt::Horizontal, tr("Benutzer"));
+    setHeaderData(2, Qt::Horizontal, tr("Gruppe"));
+}
