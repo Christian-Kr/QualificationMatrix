d3822fbfe86ca48f795678fe5c78097ef8e7ec70
Christian Kr <CerebrosuS@gmx.net>
2023-09-02 09:23:18 +0200

    add admin flag to model

--- src/ams/model/qmamsusermodel.cpp
+++ src/ams/model/qmamsusermodel.cpp
@@ -1,22 +1,19 @@
 // qmamsusermodel.cpp is part of QualificationMatrix
 //
-// QualificationMatrix is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by the
-// Free Software Foundation, either version 3 of the License, or (at your
-// option) any later version.
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
 //
-// QualificationMatrix is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-// more details.
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+// the GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along
-// with QualificationMatrix. If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
 
 #include "qmamsusermodel.h"
 
 #include <QColor>
-#include <QDebug>
 
 QMAMSUserModel::QMAMSUserModel(QObject *parent, const QSqlDatabase &db)
     : QMSqlTableModel(parent, db)
@@ -40,6 +37,7 @@ void QMAMSUserModel::initModel()
     setHeaderData(4, Qt::Horizontal, tr("Letzter Login"));
     setHeaderData(5, Qt::Horizontal, tr("Loginversuche"));
     setHeaderData(6, Qt::Horizontal, tr("Aktiv"));
+    setHeaderData(7, Qt::Horizontal, tr("Admin"));
 }
 
 QVariant QMAMSUserModel::data(const QModelIndex &index, int role) const
