a5cdfe9e0b904e5ac1462b0f6fdb53b04991c5f6
Christian Kr <CerebrosuS@gmx.net>
2023-09-02 09:21:00 +0200

    change comments

--- src/ams/qmamsusersettingswidget.cpp
+++ src/ams/qmamsusersettingswidget.cpp
@@ -1,15 +1,15 @@
 // qmamssettingswidget.cpp is part of QualificationMatrix
 //
-// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms of the GNU General
-// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
+// QualificationMatrix is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
 //
-// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-// more details.
+// QualificationMatrix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+// the GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along with QualificationMatrix. If not, see
-// <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along with QualificationMatrix.
+// If not, see <http://www.gnu.org/licenses/>.
 
 #include "qmamsusersettingswidget.h"
 #include "ui_qmamsusersettingswidget.h"
@@ -25,8 +25,6 @@
 #include <QMessageBox>
 #include <QSortFilterProxyModel>
 #include <QInputDialog>
-
-#include <QDebug>
 
 QMAMSUserSettingsWidget::QMAMSUserSettingsWidget(QWidget *parent)
     : QMSettingsWidget(parent, true)
