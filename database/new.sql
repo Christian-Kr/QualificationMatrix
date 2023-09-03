6559af75c461afcca46ab2a736e05c52a96959c4
Christian Kr <CerebrosuS@gmx.net>
2023-09-03 10:18:01 +0200

    formatting

--- database/new.sql
+++ database/new.sql
@@ -442,30 +442,30 @@ SELECT DISTINCT
     Employee.id as employee_id
 FROM
     AMSUser, AMSGroup, AMSUserGroup, AMSGroupEmployee, Employee
-                                                           LEFT OUTER JOIN AMSUserGroup AMSUserGroup1 ON
-            AMSUser.amsuser_id = AMSUserGroup1.amsusergroup_user AND
-            AMSGroup.amsgroup_id = AMSUserGroup1.amsusergroup_group
-                                                           LEFT OUTER JOIN AMSGroupEmployee AMSGroupEmployee1 ON
-            AMSGroup.amsgroup_id = AMSGroupEmployee1.amsgroupemployee_group AND
-            Employee.id = AMSGroupEmployee1.amsgroupemployee_employee
+LEFT OUTER JOIN AMSUserGroup AMSUserGroup1 ON
+    AMSUser.amsuser_id = AMSUserGroup1.amsusergroup_user AND
+    AMSGroup.amsgroup_id = AMSUserGroup1.amsusergroup_group
+LEFT OUTER JOIN AMSGroupEmployee AMSGroupEmployee1 ON
+    AMSGroup.amsgroup_id = AMSGroupEmployee1.amsgroupemployee_group AND
+    Employee.id = AMSGroupEmployee1.amsgroupemployee_employee
 WHERE
-        AMSGroup.amsgroup_id = AMSUserGroup.amsusergroup_group AND
-        AMSUser.amsuser_id = AMSUserGroup.amsusergroup_user AND
-        AMSGroup.amsgroup_id = AMSGroupEmployee.amsgroupemployee_group AND
-        Employee.id = AMSGroupEmployee.amsgroupemployee_employee;
+    AMSGroup.amsgroup_id = AMSUserGroup.amsusergroup_group AND
+    AMSUser.amsuser_id = AMSUserGroup.amsusergroup_user AND
+    AMSGroup.amsgroup_id = AMSGroupEmployee.amsgroupemployee_group AND
+    Employee.id = AMSGroupEmployee.amsgroupemployee_employee;
 
 
 -- Create table for ams groups and employee groups relation
 
 CREATE TABLE IF NOT EXISTS "AMSGroupEmployeeGroup" (
-                                                       "amsgroupemployeegroup_id"                           INTEGER,
-                                                       "amsgroupemployeegroup_group"                        INTEGER,
-                                                       "amsgroupemployeegroup_employeegroup"                INTEGER,
-                                                       PRIMARY KEY("amsgroupemployeegroup_id"),
-                                                       FOREIGN KEY("amsgroupemployeegroup_group")           REFERENCES
-                                                           "AMSGroup" ("amsgroup_id"),
-                                                       FOREIGN KEY("amsgroupemployeegroup_employeegroup")   REFERENCES
-                                                           "EmployeeGroup" ("id")
+    "amsgroupemployeegroup_id"                           INTEGER,
+    "amsgroupemployeegroup_group"                        INTEGER,
+    "amsgroupemployeegroup_employeegroup"                INTEGER,
+    PRIMARY KEY("amsgroupemployeegroup_id"),
+    FOREIGN KEY("amsgroupemployeegroup_group")           REFERENCES
+        "AMSGroup" ("amsgroup_id"),
+    FOREIGN KEY("amsgroupemployeegroup_employeegroup")   REFERENCES
+        "EmployeeGroup" ("id")
 );
 
 
@@ -478,9 +478,9 @@ SELECT DISTINCT
 FROM
     AMSUser, AMSGroup, AMSUserGroup, AMSGroupEmployeeGroup, EmployeeGroup, Employee
 WHERE
-        AMSUser.amsuser_id = AMSUserGroup.amsusergroup_user AND
-        AMSGroup.amsgroup_id = AMSUserGroup.amsusergroup_group AND
-        AMSGroup.amsgroup_id = AMSGroupEmployeeGroup.amsgroupemployeegroup_group AND
-        EmployeeGroup.id = AMSGroupEmployeeGroup.amsgroupemployeegroup_employeegroup AND
-        Employee.'group' = EmployeeGroup.id;
+    AMSUser.amsuser_id = AMSUserGroup.amsusergroup_user AND
+    AMSGroup.amsgroup_id = AMSUserGroup.amsusergroup_group AND
+    AMSGroup.amsgroup_id = AMSGroupEmployeeGroup.amsgroupemployeegroup_group AND
+    EmployeeGroup.id = AMSGroupEmployeeGroup.amsgroupemployeegroup_employeegroup AND
+    Employee.'group' = EmployeeGroup.id;
 
